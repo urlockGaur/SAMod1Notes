10de1288c189895a8f414506c4eab2ac1976b5c2
Stacy Read <stacy.read.007@gmail.com>
2020-02-16 18:01:20 -0600

    Whoops, CrocodileAdapter didn't compile

--- src/indirection/CrocodileAdapter.java
+++ src/indirection/CrocodileAdapter.java
@@ -1,19 +1,16 @@
 package indirection;
 
-import lowcoupling.Animal;
-
 /**
  * This example of indirection uses the Adapter design pattern.
  * The CrocodileAdapter extends Animal, so it can be added
  * to Zoos. It wraps a Crocodile object, allowing it be treated
  * like an Animal.
  */
-public class CrocodileAdapter extends Animal {
+public class CrocodileAdapter {
     
     private Crocodile c;
 
     public CrocodileAdapter(Crocodile c){
-        super(c.getName());
         this.c = c;
     }
 
