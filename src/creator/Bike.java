76070d5901bd9e5929ee7ce7a1e850affec78ccd
Stacy Read <sread@wctc.edu>
2022-03-22 13:46:53 -0500

    Fix typo

--- src/creator/Bike.java
+++ src/creator/Bike.java
@@ -5,7 +5,7 @@ public class Bike {
     private Wheel frontWheel, backWheel;
     private Frame frame;
 
-    /* Finish the Bike class. You should be able create a Bike
+    /* Finish the Bike class. You should be able to create a Bike
     object with two Wheel objects and a Frame object*/
 
 }
